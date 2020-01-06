f98e446f66406ce71891ed6124c80c26993a4864
IvyCharon <shihanwen@sjtu.edu.cn>
2020-01-06 23:06:18 +0800

    ying

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ using namespace antlr4;
 //       if you really need to regenerate,please ask TA for help.
 int main(int argc, const char* argv[]){
     //todo:please don't modify the code below the construction of ifs if you want to use visitor mode
-    const std::string filepath="testData/test"+std::string(argv[1])+".in";
+    //const std::string filepath="testData/test"+std::string(argv[1])+".in";
     /*const std::string filepath="/mnt/c/Users/skysz/Desktop/study/c/pythonInterpreter/Python_Assignment-master/testData/test9.in";
     std::ifstream ifs;
     ifs.open(filepath);
